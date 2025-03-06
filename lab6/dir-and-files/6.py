77ca74818e2ff04ea3c71c00b40673e925c45c9a
ROCK032 <138594361+ROCK032@users.noreply.github.com>
2025-03-06 14:54:40 +0500

    Update 6.py

--- lab6/dir-and-files/6.py
+++ lab6/dir-and-files/6.py
@@ -1,9 +1,12 @@
+import os
 import string
 
+directory = input("Enter the directory to save files: ")
 
-text_list = list(string.ascii_uppercase)
-
-
-for letter in text_list:
-    with open(f"{letter}.txt", "w") as f:
-        f.write(f"This is file {letter}")
+if not os.path.exists(directory):
+    os.makedirs(directory)
+for i in string.ascii_uppercase:
+    file_path = os.path.join(directory, f'{i}.txt')
+    with open(file_path, "w") as file:
+        file.write(f"This is file {i}.txt\n")
+print("26 FILES created")
