a6d31202520ac09766f5e1e72d4e6e06e4c930ff
ROCK032 <138594361+ROCK032@users.noreply.github.com>
2025-03-06 13:30:06 +0500

    Update 2.py

--- lab6/dir-and-files/2.py
+++ lab6/dir-and-files/2.py
@@ -1,22 +1,11 @@
 import os
 
+path = input("Enter the folder path\n")
 
-def main(path):
-    result = {
-        "exists": os.path.exists(path),
-        "readable": os.access(path, os.R_OK),
-        "writable": os.access(path, os.W_OK),
-        "executable": os.access(path, os.X_OK)
-    }
-    return result
-
-
-if __name__ == "__main__":
-    path = input("Введите путь: ")
-    access_info = main(path)
-
-    print("Результаты проверки пути:")
-    print(f"Существует: {'Да' if access_info['exists'] else 'Нет'}")
-    print(f"Доступен для чтения: {'Да' if access_info['readable'] else 'Нет'}")
-    print(f"Доступен для записи: {'Да' if access_info['writable'] else 'Нет'}")
-    print(f"Доступен для выполнения: {'Да' if access_info['executable'] else 'Нет'}")
+if os.path.exists(path):
+    print("Path exists")
+    print("Readable", "Yes" if os.access(path, os.R_OK) else "No")
+    print("Writable", "Yes" if os.access(path, os.W_OK) else "No")
+    print("Executable", "Yes" if os.access(path, os.X_OK) else "No")
+else:
+    print("Path does not exist")
