103e7a0cf09d6ec1786ed3ba6edd86e76a96b36a
ROCK032 <138594361+ROCK032@users.noreply.github.com>
2025-03-06 15:13:40 +0500

    Update 8.py

--- lab6/dir-and-files/8.py
+++ lab6/dir-and-files/8.py
@@ -1,17 +1,10 @@
 import os
 
-def delete_file(file_path):
-    if os.path.exists(file_path):
-        if os.access(file_path, os.W_OK):
-            try:
-                os.remove(file_path)
-                print(f"Файл '{file_path}' успешно удалён.")
-            except Exception as e:
-                print(f"Ошибка при удалении файла: {e}")
-        else:
-            print(f"Нет прав на удаление файла '{file_path}'.")
-    else:
-        print(f"Файл '{file_path}' не существует.")
+path = input("Enter the file path to delete\n")
 
-file_path = input("Введите полный путь к файлу: ")
-delete_file(file_path)
+if os.path.isfile(path) and os.access(path, os.W_OK):
+    os.remove(path)
+    print("File deleted.")
+else:
+    print("Cannot delete file.")
+
