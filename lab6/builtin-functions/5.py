df6677230483429af8518f302f3dc6c787c09772
ROCK032 <138594361+ROCK032@users.noreply.github.com>
2025-03-06 18:28:17 +0500

    Update 5.py

--- lab6/builtin-functions/5.py
+++ lab6/builtin-functions/5.py
@@ -1,7 +1,7 @@
 def main(n):
     return all(n)
 
-my_tuple = (True, 1, "Hello", [1, 2])
+my_tuple = (True, 1)
 print(main(my_tuple))
-my_tuple2 = (True, 0, "Hello")
+my_tuple2 = (True, 0)
 print(main(my_tuple2))
