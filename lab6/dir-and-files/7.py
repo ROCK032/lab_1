d17c5579d69057f4a0de177cdbf494ef1799fba7
ROCK032 <138594361+ROCK032@users.noreply.github.com>
2025-03-06 15:09:00 +0500

    Update 7.py

--- lab6/dir-and-files/7.py
+++ lab6/dir-and-files/7.py
@@ -1,10 +1,10 @@
 import shutil
 
-source = input("Enter the source file path: ")
-destination = input("Enter the destination file path: ")
+file1 = input("Enter the file1 file path: ")
+file2 = input("Enter the file2 file path: ")
 
 try:
-    shutil.copyfile(source, destination)
+    shutil.copyfile(file1, file2)
     print("File copied successfully.")
 except FileNotFoundError:
     print("Error: Source file not found.")
@@ -12,4 +12,3 @@ except PermissionError:
     print("Error: Permission denied.")
 except Exception as e:
     print(f"An error occurred: {e}")
-
